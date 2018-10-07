9344757663d5113ca47a8584b69af89e98bb3423
Natalia Romankevich <40891986+natBizitza@users.noreply.github.com>
2018-10-07 19:47:28 +0200

    first part

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+
+            Console.WriteLine("Please, introduce your name");
+            name = Console.ReadLine();
+
+            Console.WriteLine("What is your age?");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Client " + name + " " + age + " years old.");
+
+            Console.ReadLine();
+
+        }
+    }
+}
